5dd32656891dbabea9077b0b70d96a3b5d785977
Vance97 <vansmuchongo@gmail.com>
2023-02-21 20:59:24 +0200

    fix 10

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,8 +2,8 @@
 #include "6-abs.c"
 #include <stdio.h>
 /**
- * Description: 'prints from 0 to 98'
- * Takes parameter n
+ * main - Entry point
+ * Description: 'print_to_98'
  * Return: Always 0 (Success)
  */
 void print_to_98(int n)
